f2429a3986c5dd7c834635a1ccac254338c0cd28
Deepdarshan <deepdarshan21@gmail.com>
2022-08-25 10:19:54 +0530

    feat: add button to get score and dropdown for select type of identity
    Signed-off-by: Deepdarshan <deepdarshan21@gmail.com>

--- pages/index.js
+++ pages/index.js
@@ -3,9 +3,9 @@ import Image from "next/image";
 import { useEffect } from "react";
 
 export default function Home() {
-    // const handleClick = (evt) =>{
-
-    // }
+    const getScore = (evt) => {
+        evt.preventDefault();
+    };
 
     useEffect(() => {
         document.querySelectorAll(".drop-zone__input").forEach((inputElement) => {
@@ -57,7 +57,7 @@ export default function Home() {
                 </div>
             </nav>
 
-            <div>
+            <div className="flex flex-col items-center space-y-10">
                 <div
                     className="drop-zone"
                     // onClick={handleClick}
@@ -69,6 +69,39 @@ export default function Home() {
                 >
                     <span className="drop-zone__prompt">Drop file here or click to upload</span>
                     <input type="file" name="myFile" className="drop-zone__input" />
+                </div>
+
+                <div className="flex flex-col items-center space-y-4">
+                    <span>
+                        <select
+                            id="dropdownDefault"
+                            data-dropdown-toggle="dropdown"
+                            className="text-white bg-blue-600 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm pl-4 pr-5 py-2.5 text-center inline-flex items-center"
+                            type="button"
+                        >
+                            <option disabled className="block py-2 text-center hover:bg-gray-100">
+                                Proof of
+                            </option>
+                            <option className="block py-2 text-center hover:bg-gray-100">
+                                Proof of Identity
+                            </option>
+                            <option className="block py-2 text-center hover:bg-gray-100">
+                                Proof of Address
+                            </option>
+                            <option className="block py-2 text-center hover:bg-gray-100">
+                                Proof of Birth
+                            </option>
+                        </select>
+                    </span>
+
+                    <span>
+                        <button
+                            onClick={getScore}
+                            className="block py-2 px-4 bg-white border-2 rounded-xl border-blue-800 hover:bg-blue-600"
+                        >
+                            Get Score
+                        </button>
+                    </span>
                 </div>
             </div>
         </div>
